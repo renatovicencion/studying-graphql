38f25c3585d07a98840426e53996eeaf56d0ca41
renatovicencion <renatovicencion@gmail.com>
2023-03-03 12:22:14 -0300

    Testing if vercel works

--- src/graphql/resolvers/resolversMap.ts
+++ src/graphql/resolvers/resolversMap.ts
@@ -1,7 +1,14 @@
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeResolvers } from "@graphql-tools/merge";
+import character from "./character";
+import developer from "./developer";
+import game from "./game";
+import person from "./person";
 
-const resolversFiles = loadFilesSync(__dirname);
+// VERCEL NO SOPORTA  dynamic loaders COMO ESTE, ASÍ QUE DEBO CAMBIARLO
+// const resolversFiles = loadFilesSync(__dirname);
+
+const resolversFiles = [character, developer, game, person];
 
 const resolvers = mergeResolvers(resolversFiles);
 
