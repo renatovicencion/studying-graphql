1d9b3e79664f33ea6a6deb220b756c2e8e9583c0
renatovicencion <renatovicencion@gmail.com>
2023-03-03 12:13:28 -0300

    Testing if vercel works

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -4,13 +4,19 @@ import { loadSchemaSync } from "@graphql-tools/load";
 import "graphql-import-node";
 import resolvers from "./resolvers/resolversMap";
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import character from "./schemas/character.graphql";
+import developer from "./schemas/developer.graphql";
+import game from "./schemas/game.graphql";
+import person from "./schemas/person.graphql";
 
 // VERCEL NO SOPORTA  dynamic loaders COMO ESTE, ASÍ QUE DEBO CAMBIARLO
-const typeDefs = loadSchemaSync("./**/*.graphql", {
-	loaders: [new GraphQLFileLoader()],
-});
+// const typeDefs = loadSchemaSync("./**/*.graphql", {
+// 	loaders: [new GraphQLFileLoader()],
+// });
+
+const typeDefs = [character, developer, game, person];
 
 export const schema: GraphQLSchema = mergeSchemas({
-	schemas: [typeDefs],
+	typeDefs: typeDefs,
 	resolvers,
 });
